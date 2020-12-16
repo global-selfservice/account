b6f742d8c57be06bb38b769ceedbbec172afdcd1
Vinod Sai Kumar <60657267+vinod-global@users.noreply.github.com>
2020-12-16 09:28:51 +0000

    feat: added ecr policies for worker roles (#5)
    * feat: added ecr policies for worker roles
    
    * feat: fixed the validate error

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,8 @@
 locals {
+  prod_workers = toset([
+    module.common.cluster_worker_roles["selfservice-preprod"],
+    module.common.cluster_worker_roles["selfservice-prod"]
+  ])
 
   repos = toset([
     "selfservice-products-api",
@@ -43,4 +47,45 @@ resource "aws_ecr_lifecycle_policy" "policy" {
   repository = aws_ecr_repository.repo[each.key].name
 
   policy = local.policy_string
+}
+
+resource "aws_ecr_repository_policy" "policy" {
+  for_each = local.ecr_repos
+
+  repository = aws_ecr_repository.repo[each.key].name
+  policy     = data.aws_iam_policy_document.ecr[each.key].json
+}
+
+data "aws_iam_policy_document" "ecr" {
+  for_each = local.ecr_repos
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart"
+    ]
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = local.prod_workers
+    }
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+  }
 }
