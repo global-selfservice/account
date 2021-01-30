37214ba95d6460eb1606ada836e407ff94264a24
Stuart Clark <stuart.clark@Jahingo.com>
2021-01-30 00:07:54 +0000

    fix: Remove cost role (#25)

--- role.tf
+++ role.tf
@@ -10,9 +10,3 @@ module "ADFS" {
 
   provider_id = "arn:aws:iam::${local.account_id}:saml-provider/TIG_ADFS"
 }
-
-module "cost" {
-  source = "github.com/global-devops-terraform/cost-role?ref=v1.3.0"
-
-  cost_role = module.common.cost_lambda_role
-}
